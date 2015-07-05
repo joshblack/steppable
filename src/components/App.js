666dc17c8ce26f8796684e98714e18a7705c135a
Josh Black <joshblack@us.ibm.com>
2015-07-05 17:43:47 -0500

    Adding another transform

--- src/components/App.js
+++ src/components/App.js
@@ -7,29 +7,36 @@ import { Component } from 'react';
 // Will give you a timeline, contributing factor of each affect, and a preview
 // maybe give a predictor?
 //
+// Maybe give option to keep adding a translation or a scale and see what happens
+//
 // translate
 // scale
 // rotate
 export default class App extends Component {
   constructor() {
     super();
-    this.state = { to: { x: 400, y: 400 } }
+
+    this.state = {
+      to: { x: 400, y: 400 },
+      scale: { x: 2, y: 2 }
+    };
+
     this.handleOnclick = this.handleOnclick.bind(this);
   }
 
   handleOnclick() {
     if (this.state.to.x === 400)
-      this.setState({ to: { x: 0, y: 0 } });
+      this.setState({ to: { x: 0, y: 0 }, scale: { x: 1, y: 1 } });
     else
-      this.setState({ to: { x: 400, y: 400 } });
+      this.setState({ to: { x: 400, y: 400 }, scale: { x: 2, y: 2} });
   }
 
   render () {
     return (
       <section>
         <button onClick={this.handleOnclick}>Click me</button>
-        <Stepper to={this.state.to}>
-          {({x, y}) => <Box offset={{x, y}} />}
+        <Stepper to={this.state.to} scale={this.state.scale}>
+          {transform => <Box transform={transform} />}
         </Stepper>
       </section>
     );
@@ -40,12 +47,19 @@ function interpolate(x, y, t) {
   return x + t * (y - x);
 }
 
+//  matrix(1, 0, 0, 1, 399.998413085938, 399.998413085938);
+
 const step = 0.01;
 
 class Stepper extends Component {
   constructor() {
     super();
-    this.state = { offset: { x: 0, y: 0 }};
+
+    this.state = {
+      offset: { x: 0, y: 0 },
+      scale: { x: 1, y: 1 }
+    };
+
     this.renderFrame = this.renderFrame.bind(this);
   }
 
@@ -58,10 +72,15 @@ class Stepper extends Component {
         y: interpolate(this.state.offset.y, this.props.to.y, t)
       };
 
+      const scale = {
+        x: interpolate(this.state.scale.x, this.props.scale.x, t),
+        y: interpolate(this.state.scale.y, this.props.scale.y, t)
+      };
+
       if (Math.abs(offset.x - this.props.to.x) <= 0.001) {
         cancelAnimationFrame(this.raf);
       } else {
-        this.setState({ offset });
+        this.setState({ offset, scale });
         this.renderFrame(t + step);
       }
     });
@@ -84,8 +103,20 @@ class Stepper extends Component {
     // Make child just expose a transform property that it then hooks into?
     // In future can abstract components that have certain animations
     // and children passed into these components will be animated that way
-    return this.props.children(this.state.offset);
+    return this.props.children(
+      CSSmatrix(
+        this.state.scale.x,
+        0,
+        0,
+        this.state.scale.y,
+        this.state.offset.x,
+        this.state.offset.y
+      ));
   }
+}
+
+function CSSmatrix(...values) {
+  return `matrix(${values.join(', ')})`;
 }
 
 // Abstract this to render children and have this control
@@ -97,7 +128,7 @@ class Box {
         background: 'black',
         width: 100,
         height: 100,
-        transform: `translate3d(${this.props.offset.x}px, ${this.props.offset.y}px, 0)`
+        transform: this.props.transform
       }} />
     );
   }
